45763db2386f25d70ecb77198da91be35a509971
LenaTrang <filename.trang@gmail.com>
2022-09-21 16:50:50 -0400

    added files for loading in bag

---
+++ bag2mat.m
@@ -0,0 +1,15 @@
+bag_name = "velodyne_sys_id_trajectories.bag";
+bag_file = rosbag(bag_name);
+num_topics = height(bag_file.AvailableTopics);
+publisher_map = containers.Map;
+messageStruct_map = containers.Map;
+topic_list = [];
+
+for i=1:num_topics
+    curr_topic = bag_file.AvailableTopics.Properties.RowNames{i};
+    topic_list = [topic_list, curr_topic];
+    curr_topic_type = string(bag_file.AvailableTopics.MessageType(i));
+    bSel = select(bag_file,'Topic',curr_topic);
+    msgStructs = readMessages(bSel, 'DataFormat','struct');
+    messageStruct_map(curr_topic) = msgStructs;
+end
---
+++ loadMatFIle.m
@@ -0,0 +1,15 @@
+clear all;
+close all;
+clc;
+map = load('velodyne_sys_id_trajectories.mat');
+imu = cell2mat(map.messageStruct_map('/imu/data'));
+joint_states = cell2mat(map.messageStruct_map('/joint_states'));
+state = cell2mat(map.messageStruct_map('/state_estimator/states'));
+rover_debug_state_out = cell2mat(map.messageStruct_map('/state_out/rover_debug_state_out'));
+tf = cell2mat(map.messageStruct_map('/tf'));
+current = cell2mat(map.messageStruct_map('/vesc/commands/motor/current'));
+unsmoothed_speed = cell2mat(map.messageStruct_map('/vesc/commands/motor/unsmoothed_speed'));
+odom = cell2mat(map.messageStruct_map('/vesc/odom'));
+vesc_sensors_core = cell2mat(map.messageStruct_map('/vesc/sensors/core'));
+servo_position_command = cell2mat(map.messageStruct_map('/vesc/sensors/servo_position_command'));
+
