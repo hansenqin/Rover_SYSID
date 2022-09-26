527c87e8a35a20ed2d90df5d133296149815459b
Vishrut Kaushik <vishrutskaushik@gmail.com>
2022-09-26 15:45:33 -0400

    Synced time between poses, velocities and acceleartion | Plots look good

--- tire_curve_sysID_helper_class.m
+++ tire_curve_sysID_helper_class.m
@@ -32,8 +32,11 @@ classdef tire_curve_sysID_helper_class < handle
         vehicle_motor_current_command = struct('time', 0, 'motor_current', 0);
         desired_states = struct('time', 0, 'ud', 0, 'vd', 0, 'rd', 0);
         auto_flag_data = struct('time', 0, 'auto_flag', 0);
-        vehicle_states_from_slam = struct('time', 0, 'x', 0, 'y', 0, 'h', 0);
-        vehicle_states_derived_from_slam = struct('u', 0, 'v', 0, 'r', 0, 'udot', 0, 'vdot', 0, 'rdot', 0);
+        vehicle_pose_from_slam = struct('time', 0, 'x', 0, 'y', 0, 'h', 0);
+        vehicle_velocities_derived_from_slam = struct('time', 0, 'u', 0, 'v', 0, 'r', 0);
+        vehicle_acceleration_derived_from_slam = struct('time', 0, 'udot', 0, 'vdot', 0, 'rdot', 0);
+        
+        vehicle_states_from_slam = struct('time', 0, 'u', 0, 'v', 0, 'r', 0, 'udot', 0, 'vdot', 0, 'rdot', 0);
         
         % signal stuff
         start_time_offset;
@@ -76,13 +79,14 @@ classdef tire_curve_sysID_helper_class < handle
             
             %Load data
             load_vehicle_states_data(obj);
-            load_vehicle_states_from_slam_data(obj);
+            load_vehicle_pose_from_slam_data(obj);
             load_zonotope_data(obj);
-            load_commands(obj)
+            load_commands(obj);
             load_imu_data(obj);
             load_param_gen_data(obj);
             load_auto_flag(obj);
             compute_velocity_and_acceleration_from_slam_data(obj, true);
+
             
             % Sync signals
             % reference_topic is used as the standard sampling
@@ -91,8 +95,10 @@ classdef tire_curve_sysID_helper_class < handle
             reference_topic = '/state_out/rover_debug_state_out';
             set_standard_time(obj, reference_topic);
 %             structs_to_sync = ["vehicle_states", "vehicle_delta_command", "vehicle_motor_current_command", "desired_states", "auto_flag_data"];
-            structs_to_sync = ["vehicle_states", "vehicle_motor_current_command", "vehicle_encoder","desired_states", "auto_flag_data"];
-            synchronize_signals_sample_rate(obj, structs_to_sync);
+%             structs_to_sync = ["vehicle_states", "vehicle_motor_current_command", "vehicle_encoder","desired_states", "auto_flag_data"];
+            
+            structs_to_sync = ["vehicle_pose_from_slam", "vehicle_velocities_derived_from_slam", "vehicle_acceleration_derived_from_slam", "vehicle_encoder", "vehicle_motor_current_command"];
+%             synchronize_signals_sample_rate(obj, structs_to_sync);
             
             % Low pass signals
             structs_to_low_pass = ["vehicle_states"];
@@ -131,7 +137,7 @@ classdef tire_curve_sysID_helper_class < handle
                 field_names = fieldnames(obj.(structs(i)));
                 time = obj.(structs(i)).time;
                 for j = 2:length(field_names)
-                    obj.(structs(i)).(field_names{j}) = interp1(time(:),obj.(structs(i)).(field_names{j}),obj.standard_time(:),'linear'); % 'extrap'
+                    obj.(structs(i)).(field_names{j}) = interp1(time(:),obj.(structs(i)).(field_names{j}),obj.standard_time(:),'linear', 'extrap'); % 'extrap'
                 end
                 obj.(structs(i)).time = obj.standard_time(:);
             end
@@ -196,64 +202,98 @@ classdef tire_curve_sysID_helper_class < handle
             obj.vehicle_encoder.encoder_velocity = cell2mat(cellfun(@(s)s.Velocity,msgStructs,'uni',0));
        end
        
-       function obj = load_vehicle_states_from_slam_data(obj)
+       function obj = load_vehicle_pose_from_slam_data(obj)
             bSel = select(obj.bag, 'Topic', '/tf');
             tf = cell2mat(readMessages(bSel, 'DataFormat', 'struct'));
-            obj.vehicle_states_from_slam.time = [];
-            obj.vehicle_states_from_slam.x = [];
-            obj.vehicle_states_from_slam.y = [];
-            obj.vehicle_states_from_slam.h = [];
+            obj.vehicle_pose_from_slam.time = [];
+            obj.vehicle_pose_from_slam.x = [];
+            obj.vehicle_pose_from_slam.y = [];
+            obj.vehicle_pose_from_slam.h = [];
             for i = 1:size(tf)
                 if size(tf(i).Transforms, 2) > 0
                     if tf(i).Transforms.Header.FrameId == "map" && tf(i).Transforms.ChildFrameId == obj.robot_frame
                         time = (cast(tf(i, 1).Transforms.Header.Stamp.Sec, 'double')*1e9 + cast(tf(i, 1).Transforms.Header.Stamp.Nsec, 'double'))*1e-9;
-                        obj.vehicle_states_from_slam.time = [obj.vehicle_states_from_slam.time, time];
-                        obj.vehicle_states_from_slam.x = [obj.vehicle_states_from_slam.x, tf(i).Transforms.Transform.Translation.X];
-                        obj.vehicle_states_from_slam.y = [obj.vehicle_states_from_slam.y, tf(i).Transforms.Transform.Translation.Y];
+                        obj.vehicle_pose_from_slam.time = [obj.vehicle_pose_from_slam.time, time];
+                        obj.vehicle_pose_from_slam.x = [obj.vehicle_pose_from_slam.x, tf(i).Transforms.Transform.Translation.X];
+                        obj.vehicle_pose_from_slam.y = [obj.vehicle_pose_from_slam.y, tf(i).Transforms.Transform.Translation.Y];
                         q = [tf(i).Transforms.Transform.Rotation.W, tf(i).Transforms.Transform.Rotation.X, tf(i).Transforms.Transform.Rotation.Y, tf(i).Transforms.Transform.Rotation.Z];
                         [z, y, x] = quat2angle(q);
-                        obj.vehicle_states_from_slam.h = [obj.vehicle_states_from_slam.h, z];
+                        obj.vehicle_pose_from_slam.h = [obj.vehicle_pose_from_slam.h, z];
                     end
                 end
             end
+            
+            obj.vehicle_pose_from_slam.x = smoothdata(obj.vehicle_pose_from_slam.x, 'gaussian', 20);
+            obj.vehicle_pose_from_slam.y = smoothdata(obj.vehicle_pose_from_slam.y, 'gaussian', 15);
+            obj.vehicle_pose_from_slam.h = smoothdata(obj.vehicle_pose_from_slam.h, 'gaussian', 10);
        end
 
-       function obj = compute_velocity_and_acceleration_from_slam_data(obj, displayPlot)
-           x = obj.vehicle_states_from_slam.x;
-           y = obj.vehicle_states_from_slam.y;
-           h = obj.vehicle_states_from_slam.h;
-           time = obj.vehicle_states_from_slam.time;
+       function obj = compute_velocity_from_slam_data(obj)
+           x = obj.vehicle_pose_from_slam.x;
+           y = obj.vehicle_pose_from_slam.y;
+           h = obj.vehicle_pose_from_slam.h;
+           time = obj.vehicle_pose_from_slam.time;
 
-           x = smoothdata(x, 'gaussian', 20);
-           y = smoothdata(y, 'gaussian', 15);
-           h = smoothdata(h, 'gaussian', 10);
-            
-           u = diff(x) ./ diff(time);
-           v = diff(y) ./ diff(time);
-           r = diff(h) ./ diff(time);
+           u = [];
+           v = [];
+           r = [];
+           for i = 1:length(x)-1
+                u = [u, (x(i+1) - x(i)) / (time(i+1) - time(i) + 0.001)];
+                v = [v, (y(i+1) - y(i)) / (time(i+1) - time(i) + 0.001)];
+                r = [r, (h(i+1) - h(i)) / (time(i+1) - time(i) + 0.001)];
+           end
 
            u = smoothdata(u, 'gaussian', 20);
            v = smoothdata(v, 'gaussian', 20);
            r = smoothdata(r, 'gaussian', 20);
 
-           udot = diff(u) ./ diff(time(1:end-1));
-           vdot = diff(v) ./ diff(time(1:end-1));
-           rdot = diff(r) ./ diff(time(1:end-1));
+           obj.vehicle_velocities_derived_from_slam.u = u;
+           obj.vehicle_velocities_derived_from_slam.v = v;
+           obj.vehicle_velocities_derived_from_slam.r = r;
+           obj.vehicle_velocities_derived_from_slam.time = time(1:end-1);
+       end
+
+       function obj = compute_acceleration_from_slam_data(obj)
+           u = obj.vehicle_velocities_derived_from_slam.u;
+           v = obj.vehicle_velocities_derived_from_slam.v;
+           r = obj.vehicle_velocities_derived_from_slam.r;
+           time = obj.vehicle_velocities_derived_from_slam.time;
+
+           udot = [];
+           vdot = [];
+           rdot = [];
+           for i = 1:length(u)-1
+                udot = [udot, (u(i+1) - u(i)) / (time(i+1) - time(i) + 0.001)];
+                vdot = [vdot, (v(i+1) - v(i)) / (time(i+1) - time(i) + 0.001)];
+                rdot = [rdot, (r(i+1) - r(i)) / (time(i+1) - time(i) + 0.001)];
+           end
 
            udot = smoothdata(udot, 'gaussian', 20);
            vdot = smoothdata(vdot, 'gaussian', 20);
            rdot = smoothdata(rdot, 'gaussian', 20);
 
-           obj.vehicle_states_from_slam.x = x;
-           obj.vehicle_states_from_slam.y = y;
-           obj.vehicle_states_from_slam.h = h;
-           obj.vehicle_states_derived_from_slam.u = u;
-           obj.vehicle_states_derived_from_slam.v = v;
-           obj.vehicle_states_derived_from_slam.r = r;
-           obj.vehicle_states_derived_from_slam.udot = udot;
-           obj.vehicle_states_derived_from_slam.vdot = vdot;
-           obj.vehicle_states_derived_from_slam.rdot = rdot;
+           obj.vehicle_acceleration_derived_from_slam.udot = udot;
+           obj.vehicle_acceleration_derived_from_slam.vdot = vdot;
+           obj.vehicle_acceleration_derived_from_slam.rdot = rdot;
+           obj.vehicle_acceleration_derived_from_slam.time = time(1:end-2);
+       end
+       
+       function obj = compute_velocity_and_acceleration_from_slam_data(obj, displayPlot)
+           obj.compute_velocity_from_slam_data();
+           obj.compute_acceleration_from_slam_data();
 
+%            To sync time
+           obj.vehicle_states_from_slam.x = obj.vehicle_pose_from_slam.x(1:end-2);
+           obj.vehicle_states_from_slam.y = obj.vehicle_pose_from_slam.y(1:end-2);
+           obj.vehicle_states_from_slam.h = obj.vehicle_pose_from_slam.h(1:end-2);
+
+           obj.vehicle_states_from_slam.u = obj.vehicle_velocities_derived_from_slam.u(1:end-1);
+           obj.vehicle_states_from_slam.v = obj.vehicle_velocities_derived_from_slam.v(1:end-1);
+           obj.vehicle_states_from_slam.r = obj.vehicle_velocities_derived_from_slam.r(1:end-1);
+
+           obj.vehicle_states_from_slam.udot = obj.vehicle_acceleration_derived_from_slam.udot;
+           obj.vehicle_states_from_slam.vdot = obj.vehicle_acceleration_derived_from_slam.vdot;
+           obj.vehicle_states_from_slam.rdot = obj.vehicle_acceleration_derived_from_slam.rdot;
            if displayPlot == true
               obj.plot_tracking_data();
            end
@@ -262,23 +302,23 @@ classdef tire_curve_sysID_helper_class < handle
        function plot_tracking_data(obj)
             figure(1);
             hold on;
-            plot(obj.vehicle_states_from_slam.x, 'r', 'DisplayName', 'SLAM X Smoothened', 'LineWidth', 2)
-            plot(obj.vehicle_states_derived_from_slam.u, 'b', 'DisplayName', 'SLAM U Smoothened', 'LineWidth', 2)
-            plot(obj.vehicle_states_derived_from_slam.udot, 'k', 'DisplayName', 'SLAM UDot Smoothened', 'LineWidth', 2)
+            plot(obj.vehicle_states_from_slam.x, 'r', 'DisplayName', 'SLAM X Smoothened', 'LineWidth', 2);
+            plot(obj.vehicle_states_from_slam.u, 'b', 'DisplayName', 'SLAM U Smoothened', 'LineWidth', 2);
+            plot(obj.vehicle_states_from_slam.udot, 'k', 'DisplayName', 'SLAM UDot Smoothened', 'LineWidth', 2);
             hold off;
             legend;
             figure(2);
             hold on;
-            plot(obj.vehicle_states_from_slam.y, 'r', 'DisplayName', 'SLAM Y Smoothened', 'LineWidth', 2)
-            plot(obj.vehicle_states_derived_from_slam.v, 'b', 'DisplayName', 'SLAM V Smoothened', 'LineWidth', 2)
-            plot(obj.vehicle_states_derived_from_slam.vdot, 'k', 'DisplayName', 'SLAM VDot Smoothened', 'LineWidth', 2)
+            plot(obj.vehicle_states_from_slam.y, 'r', 'DisplayName', 'SLAM Y Smoothened', 'LineWidth', 2);
+            plot(obj.vehicle_states_from_slam.v, 'b', 'DisplayName', 'SLAM V Smoothened', 'LineWidth', 2);
+            plot(obj.vehicle_states_from_slam.vdot, 'k', 'DisplayName', 'SLAM VDot Smoothened', 'LineWidth', 2);
             hold off;
             legend;
             figure(3);
             hold on;
-            plot(obj.vehicle_states_from_slam.h, 'r', 'DisplayName', 'SLAM H Smoothened', 'LineWidth', 2)
-            plot(obj.vehicle_states_derived_from_slam.r, 'b', 'DisplayName', 'SLAM R Smoothened', 'LineWidth', 2)
-            plot(obj.vehicle_states_derived_from_slam.rdot, 'k', 'DisplayName', 'SLAM RDot Smoothened', 'LineWidth', 2)
+            plot(obj.vehicle_states_from_slam.h, 'r', 'DisplayName', 'SLAM H Smoothened', 'LineWidth', 2);
+            plot(obj.vehicle_states_from_slam.r, 'b', 'DisplayName', 'SLAM R Smoothened', 'LineWidth', 2);
+            plot(obj.vehicle_states_from_slam.rdot, 'k', 'DisplayName', 'SLAM RDot Smoothened', 'LineWidth', 2);
             hold off;
 
             legend;
@@ -330,39 +370,21 @@ classdef tire_curve_sysID_helper_class < handle
        
 
        function [xLinearCoef, fLinearCoef, rLinearCoef, lambda, alphaf, alphar, F_x, F_yf, F_yr] = get_tire_curve_coefficients(obj)
-
-            % TODO: add curve fitting to this derivative step
-            udot = [];
-            for k=1:1:length(obj.vehicle_states.u)-1
-                newvdot = (obj.vehicle_states.u(k+1)-obj.vehicle_states.u(k))/(obj.vehicle_states.time(k+1)-obj.vehicle_states.time(k));
-                udot = [udot newvdot];
-            end
-            vdot = [];
-            for k=1:1:length(obj.vehicle_states.v)-1
-                newvdot = (obj.vehicle_states.v(k+1)-obj.vehicle_states.v(k))/(obj.vehicle_states.time(k+1)-obj.vehicle_states.time(k));
-                vdot = [vdot newvdot];
-            end
-            rdot = [];
-            for k=1:1:length(obj.vehicle_states.r)-1
-                newrdot = (obj.vehicle_states.r(k+1)-obj.vehicle_states.r(k))/(obj.vehicle_states.time(k+1)-obj.vehicle_states.time(k));
-                rdot = [rdot newrdot];
-            end
-
-            udot = udot';
-            vdot = vdot';
-            rdot = rdot';
-
-            x = obj.vehicle_states.x(1:end-1);
-            y = obj.vehicle_states.y(1:end-1);
-            h = obj.vehicle_states.h(1:end-1);
-            u = obj.vehicle_states.u(1:end-1);
-            v = obj.vehicle_states.v(1:end-1);
-            r = obj.vehicle_states.r(1:end-1);
+            x = obj.vehicle_states_from_slam.x;
+            y = obj.vehicle_states_from_slam.y;
+            h = obj.vehicle_states_from_slam.h;
+            u = obj.vehicle_states_from_slam.u;
+            v = obj.vehicle_states_from_slam.v;
+            r = obj.vehicle_states_from_slam.r;
+            udot = obj.vehicle_states_from_slam.udot;
+            vdot = obj.vehicle_states_from_slam.vdot;
+            rdot = obj.vehicle_states_from_slam.rdot;
+            
 %             w = obj.vehicle_encoder.encoder_velocity(1:end-1);
 %             w = w/(-0.5*pi*10^7);
-            w = obj.vehicle_states.w(1:end-1);
+%             w = obj.vehicle_states.w(1:end-1);
 % %             delta=obj.vehicle_delta_command.delta_cmd(1:end-1)+obj.servo_offset;
-            delta=obj.vehicle_states.delta_cmd(1:end-1);
+%             delta=obj.vehicle_states.delta_cmd(1:end-1);
         
             % Calculate the longitudinal force
             % Current method
